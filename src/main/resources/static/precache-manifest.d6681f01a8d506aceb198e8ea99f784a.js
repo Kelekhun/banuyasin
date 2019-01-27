4f18218f76ac5492087d1af22bb03fcc8923d29c
Kelekhun <khadkadeepak@gmail.com>
2019-01-26 21:41:44 -0500

    Spring React & Redux for Heroku

---
+++ src/main/resources/static/precache-manifest.d6681f01a8d506aceb198e8ea99f784a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bd57fa8efdc2c59a13d6",
+    "url": "/static/js/main.bd57fa8e.chunk.js"
+  },
+  {
+    "revision": "dc22d670ba0324fac1f1",
+    "url": "/static/js/1.dc22d670.chunk.js"
+  },
+  {
+    "revision": "bd57fa8efdc2c59a13d6",
+    "url": "/static/css/main.4e7ec8d1.chunk.css"
+  },
+  {
+    "revision": "dc22d670ba0324fac1f1",
+    "url": "/static/css/1.3ba90047.chunk.css"
+  },
+  {
+    "revision": "d743323d686ae855a1f374cc801abceb",
+    "url": "/index.html"
+  }
+];
